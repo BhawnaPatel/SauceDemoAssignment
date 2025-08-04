f6817543825a349cd00bd3ba6a8adf32cf930866
bhawna.patel <bhawna.patel@nttdata.com>
2025-08-04 17:22:25 +0100

    Added test cases for soucedemo websites

---
+++ cypress/e2e/saucedemo/cart/cart.cy.js
@@ -0,0 +1,43 @@
+import loginPage from "../../../support/pages/login.page.js";
+import inventoryPage from "../../../support/pages/inventory.page.js";
+import cartPage from "../../../support/pages/cart.page.js";
+const login = require('../../../fixtures/login.json');
+
+describe('Cart Suite', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+    loginPage.inputUsername(login.username);
+    loginPage.inputPassword(login.password);
+    loginPage.clickLoginButton();
+  })
+
+  it('Verify Success Display Cart Page with Click on Cart Icon ', () => {
+    inventoryPage.clickCartIcon();
+    cartPage.verifyCartTitleIsDisplayed();
+  });
+
+  it('Verify Success Display Product List with Click on Continue Shopping Button', () => {
+    inventoryPage.clickAddToCart();
+    inventoryPage.clickCartIcon();
+    cartPage.verifyProductListIsDisplayed();
+  });
+
+  it('Verify Success Remove a Product after Add to Cart on Cart Page', () => {
+    inventoryPage.clickAddToCart();
+    inventoryPage.clickCartIcon();
+    cartPage.clickRemoveButton();
+    cartPage.verifyProductListIsNotDisplayed();
+  });
+
+  it('Verify Success Remove a Product after Add to Cart on Product List Page', () => {
+    inventoryPage.clickAddToCart();
+    inventoryPage.clickRemove();
+    inventoryPage.verifyAddToCart();
+  });
+
+  afterEach(() => {
+    cy.clearCookies();
+  })
+
+});
---
+++ cypress/support/pages/login.page.js
@@ -0,0 +1,30 @@
+class LoginPage {
+    username = 'input[id=user-name]';
+    password = 'input[id=password]';
+    btnLogin = 'input[id=login-button]';
+    msgError = 'h3[data-test=error]';
+    loginPageUrl = 'https://www.saucedemo.com/'
+
+    inputUsername (username) {
+        cy.get(this.username).type(username);
+    }
+
+    inputPassword (password) {
+        cy.get(this.password).type(password);
+    }
+
+    clickLoginButton () {
+        cy.get(this.btnLogin).click();
+    }
+
+    errorMessageIsDisplayed (errorMsg) {
+        cy.get(this.msgError).should('have.text', errorMsg);
+    }
+
+    verifyLoginUrl () {
+        cy.url().should('equal', this.loginPageUrl);
+    }
+
+}
+
+export default new LoginPage();
